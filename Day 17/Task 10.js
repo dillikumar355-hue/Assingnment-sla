598fe9dc67735e9f718e1cdc1fc83502888fca10
dillikumar355-hue <dillikumar355@gmail.com>
2025-10-28 11:03:18 +0530

    html and js

---
+++ Day 17/Task 10.js
@@ -0,0 +1,7 @@
+var name = "Dilli"
+var a = "my_name:" + name
+console.log(a);
+
+const age = "24"
+const b = "my_age:" + age
+console.log(b);
---
+++ Day 17/Task 4.js
@@ -0,0 +1,17 @@
+// primitive operators
+var a = 10
+var b = "Dilli"
+var c = true
+var d = null
+var e
+console.log(typeof(a));
+console.log(typeof(b));
+console.log(typeof(c));
+console.log(typeof(d));
+console.log(typeof(e));
+
+// non primitive
+var f = ["Dilli", "batman", "superman"]
+const g = {my_name:"Dilli",another_name:"batman"}
+console.log(typeof(f));
+console.log(typeof(g));
