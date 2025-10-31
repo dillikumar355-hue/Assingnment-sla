55935ed3ebdcda846591f0942f4f990894639e2d
dillikumar355-hue <dillikumar355@gmail.com>
2025-10-31 10:29:56 +0530

    js

---
+++ Day 20/Task 1.js
@@ -0,0 +1,8 @@
+function printName() {
+  const name = "DILLI KUMAR";
+  for (let i = 0; i < 3; i++) {
+    console.log(name);
+  }
+}
+
+printName();
---
+++ Day 20/Task 6.js
@@ -0,0 +1,13 @@
+function getGrade(marks) {
+  if (marks >= 90) {
+    return "A";
+  } else if (marks >= 75) {
+    return "B";
+  } else if (marks >= 50) {
+    return "C";
+  } else {
+    return "F"; 
+  }
+}
+
+console.log(getGrade(95));
